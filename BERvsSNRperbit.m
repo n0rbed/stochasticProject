a27aa8f05b07c84d9839253f8fa2fbdbf06a71b9
Yassin ElBedwihy <58118886+n0rbed@users.noreply.github.com>
2025-05-09 15:35:48 +0300

    Add files via upload

--- BERvsSNRperbit.m
+++ BERvsSNRperbit.m
@@ -1,24 +1,28 @@
 % parameters 
-M = 2;   % PAM level
-num_symbols = 1e4;  % el total "symbols", i.e. time ig
-samples_per_symbol = 1;
+M = 4;   % PAM level
+num_symbols = 1e5;  % el total "symbols", i.e. time ig
+bits_per_symbol = log2(M);
 EbN0_db = -10:0.5:10;
 EbN0_linear = 10.^(EbN0_db/10);
 
+% random bits
+s = randi([0,1], num_symbols*bits_per_symbol, 1);
+s_insymbols = bit2int(s, bits_per_symbol);
+s_mapped = real(pammod(s_insymbols, M));
+signal_power = mean(abs(s_mapped).^2);
 
-s = randi([0,M-1], 1, num_symbols*samples_per_symbol);
-s_mapped = real(pammod(s, M));
 BER = zeros(size(EbN0_db));
+
 for i = 1:length(EbN0_db)
     channel = comm.AWGNChannel('EbNo', EbN0_db(i), 'BitsPerSymbol', ...
-        samples_per_symbol);
+        bits_per_symbol, 'SignalPower', signal_power);
     r = channel(s_mapped);
-    [~, BER(i)] = biterr(s, pamdemod(r, M));
+    [~, BER(i)] = biterr(s, int2bit(pamdemod(r, M), bits_per_symbol));
 end
 
-theorBER = qfunc(sqrt(2*EbN0_linear));
+theorBER = (2*(M-1))/(M*log2(M))*qfunc(sqrt(((6*log2(M))/(M^2 - 1))*EbN0_linear));
 figure;
-semilogy(EbN0_db, BER, 'bo-', 'LineWidth', 2, 'MarkerFaceColor', 'b');
+semilogy(EbN0_db, BER, 'bo-', 'LineWidth', 1, 'MarkerFaceColor', 'b');
 hold on;
 semilogy(EbN0_db, theorBER, 'r--', 'LineWidth', 2);
 grid on;
